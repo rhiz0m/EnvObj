a1f90ee8fce2f34d5341ecd0deef8844113f754a
taokyone <antonsson.andreas@hotmail.com>
2023-11-26 18:00:24 +0100

    Initial Commit

---
+++ EnvObjectProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EnvObjectProject
+//
+//  Created by Andreas Antonsson on 2023-11-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EnvObjectProject/EnvObjectProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvObjectProjectApp.swift
+//  EnvObjectProject
+//
+//  Created by Andreas Antonsson on 2023-11-26.
+//
+
+import SwiftUI
+
+@main
+struct EnvObjectProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
